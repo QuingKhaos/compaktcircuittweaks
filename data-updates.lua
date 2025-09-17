73c4c9a296d822f5b6e1e308138fc4dc42a56a69
Em Karisch <em@khaos.solutions>
2025-09-17 19:46:01 +0200

    feat: add recipe and tech overhaul setting to simplify compact processor recipes (#2)

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Remove advanced circuits and processing units from recipes, but keep their total value by adding more basic components and make the tech cheaper and earlier.
+require("data-updates.recipe-tech-overhaul")
---
+++ data-updates/recipe-tech-overhaul.lua
@@ -0,0 +1,32 @@
+if settings.startup["compaktcircuittweaks-recipe-tech-overhaul"].value then
+  for _, recipe in pairs({"compaktcircuit-processor", "compaktcircuit-processor_1x1"}) do
+    local replacement_amount = 0
+    for _, ingredient_to_remove in pairs({"advanced-circuit", "processing-unit"}) do
+      for i = 1, #data.raw.recipe[recipe].ingredients do
+        local ingredient = data.raw.recipe[recipe].ingredients[i]
+        if ingredient.name == ingredient_to_remove then
+          replacement_amount = replacement_amount + ingredient.amount
+          table.remove(data.raw.recipe[recipe].ingredients, i)
+          break
+        end
+      end
+    end
+
+    for i = 1, #data.raw.recipe[recipe].ingredients do
+      local ingredient = data.raw.recipe[recipe].ingredients[i]
+      if ingredient.name == "electronic-circuit" then
+        data.raw.recipe[recipe].ingredients[i].amount = ingredient.amount + replacement_amount
+        break
+      end
+    end
+  end
+
+  data.raw.technology["compaktcircuit-tech"].prerequisites = {"circuit-network"}
+  for i = 1, #data.raw.technology["compaktcircuit-tech"].unit.ingredients do
+    local ingredient = data.raw.technology["compaktcircuit-tech"].unit.ingredients[i]
+    if ingredient[1] == "chemical-science-pack" then
+      table.remove(data.raw.technology["compaktcircuit-tech"].unit.ingredients, i)
+      break
+    end
+  end
+end

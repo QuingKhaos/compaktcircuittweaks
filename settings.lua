f285016cb110c1d2bba080b80dab1f7b8009cb90
Em Karisch <em@khaos.solutions>
2025-09-17 19:33:57 +0200

    feat: add recipe and tech overhaul setting to simplify compact processor recipes

---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "compaktcircuittweaks-recipe-tech-overhaul",
+    setting_type = "startup",
+    default_value = false,
+    order = "aaa",
+  },
+})
